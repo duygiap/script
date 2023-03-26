e617757155d4019204bfdd0386ab794d0b16c28d
duygiap <32293786+duygiap@users.noreply.github.com>
2023-03-26 07:56:43 +0700

    Update nctvip.js

--- nctvip.js
+++ nctvip.js
@@ -1,9 +1,10 @@
 var obj = JSON.parse($response.body);
 let url = $request.url;
 var cons = "users/info";
-if(url.indexOf(cons) != -1)
-{
-obj.data.VIPExpire= "27/03/2250 00:00:00";
-obj.data.isVIP= true;
+if(url.indexOf(cons) != -1) {
+  obj.data.VIPExpire= "27/03/2250 00:00:00";
+  obj.data.isVIP= true;
+  obj.vipExpire= "01.01.2050";
+  obj.balance= "1000000 xu"
 }
 $done({body: JSON.stringify(obj)});
