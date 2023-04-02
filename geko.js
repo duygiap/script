9f594d98e9c4d90e5a1f8601195f02cd1e0a8411
duygiap <32293786+duygiap@users.noreply.github.com>
2023-04-02 14:10:43 +0700

    Update geko.js

--- geko.js
+++ geko.js
@@ -6,6 +6,5 @@ if(url.indexOf(cons) != -1) {
     'Premium';
   obj.subscription.config.hide_ads=true;
   obj.active_events.nft_rewards_program=true;
-  obj.subscription.apple_price_id=1366;
 }
 $done({body: JSON.stringify(obj)});
