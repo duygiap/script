aa1627b6414541d9cabc14b070981b23f11b9997
duygiap <32293786+duygiap@users.noreply.github.com>
2023-05-26 14:35:39 +0700

    Update sig.js

--- sig.js
+++ sig.js
@@ -2,6 +2,7 @@ var obj = JSON.parse($response.body);
 let url = $request.url;
 var cons = "Account/GetUser";
 if(url.indexOf(cons) != -1) {
-    obj.vipKind = 4
+    obj.vipKind = 4,
+    obj.vipDate= "2024-04-27T10:10:20.79"
 }
 $done({body: JSON.stringify(obj)});
