6670b0fa2866da0a024ca0d2188bda7dd1ffbf90
duygiap <32293786+duygiap@users.noreply.github.com>
2023-07-22 22:53:35 +0700

    Delete fa.js

--- fa.js
+++
@@ -1,9 +0,0 @@
-var obj = JSON.parse($response.body);
-let url = $request.url;
-var cons = "me/account";
-if(url.indexOf(cons) != -1) {
-  
-  obj.membership.level= 2;
-}
-$done({body: JSON.stringify(obj)});
-
