5c3a49e67d6dc79d009c56ac4869c7899b4fe37a
duygiap <32293786+duygiap@users.noreply.github.com>
2023-07-22 20:48:18 +0700

    Update fa.js

--- fa.js
+++ fa.js
@@ -2,7 +2,7 @@ var obj = JSON.parse($response.body);
 let url = $request.url;
 var cons = "me/account";
 if(url.indexOf(cons) != -1) {
-  obj.membership.level= 2;
-  obj.membership.endDate="2050-01-01T00:00:00";
+  obj.membership.level= 1;
+  obj.membership.endDate="2024-01-01T00:00:00";
 }
 $done({body: JSON.stringify(obj)});
