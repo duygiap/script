55aa53fb8a270641ce5fa9dc17a31996561b6e22
duygiap <32293786+duygiap@users.noreply.github.com>
2023-07-26 07:11:43 +0700

    Update fa.js

--- fa.js
+++ fa.js
@@ -7,6 +7,11 @@ if(url.indexOf(cons) != -1) {
   obj.membership.applications[1].version=3;
   obj.membership.applications[2].version=3;
   obj.membership.applications[3].version=3;
-  
+  obj.identityVerified=true;
+  obj.isTeacher=true;
+  obj.isExpert=true;
+  obj.groups=1;
+  obj.moneyAccount.availableBalance=100000000;
+  obj.moneyAccount.totalBalance=1000000000;
 }
 $done({body: JSON.stringify(obj)});
