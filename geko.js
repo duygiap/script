57613a4ae1dd826da8b0cf480ba53d5af62834a9
duygiap <32293786+duygiap@users.noreply.github.com>
2025-02-12 09:21:45 +0700

    Update geko.js

--- geko.js
+++ geko.js
@@ -4,10 +4,15 @@ let obj = JSON.parse(body);
 
 // Modify subscription details
 obj.subscription.subscription_plan_name = "Premium";
-obj.subscription.apple_price_id = "premium_plan";
-obj.subscription.start_at = "2025-01-01T00:00:00Z";
-obj.subscription.end_at = "2099-12-31T23:59:59Z";
+// obj.subscription.apple_price_id = "Premium";
+// obj.subscription.start_at = "2025-01-01T00:00:00Z";
+// obj.subscription.end_at = "2099-12-31T23:59:59Z";
+obj.subscription.config.candy_multiplier=2
 obj.subscription.config.hide_ads = true; // Disable ads
+obj.active_events.nft_rewards_program = true;
+obj.subscribed_to_newsletter = false;
+obj.show_onboarding_portfolio = true;
+
 
 // Convert modified object back to JSON
 body = JSON.stringify(obj);
