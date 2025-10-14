5cd52f9714aea3f780bd02d2ae6fcff4469bac86
duygiap <32293786+duygiap@users.noreply.github.com>
2025-10-14 11:52:36 +0700

    Update rp.js

--- rp.js
+++ rp.js
@@ -4,6 +4,6 @@ var cons = "app/v1/user/info";
 if(url.indexOf(cons) != -1) {
       ob.result.is_vip = true;
       ob.result.coin_balance = 1000000;
-      ob.result.vip_expires_at = 100000;
+      // ob.result.vip_expires_at = 100000;
 }
 $done({body: JSON.stringify(obj)});
