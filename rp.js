78bd707cc90706f57234c16bf872febed94f3609
duygiap <32293786+duygiap@users.noreply.github.com>
2025-10-14 21:54:04 +0700

    hnb

--- rp.js
+++ rp.js
@@ -1,6 +1,6 @@
 var obj = JSON.parse($response.body);
 let url = $request.url;
-var cons = "app/v1/user/info";
+var cons = "user/info";
 if(url.indexOf(cons) != -1) {
     obj.result.is_vip = true;
     obj.result.coin_balance = 1000000;
