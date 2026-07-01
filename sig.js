c2d391d17bceecd85cf334884c51fbdff3b3cd9a
duygiap <32293786+duygiap@users.noreply.github.com>
2026-07-01 23:33:47 +0700

    Update vipDate to 2028-04-27 in sig.js

--- sig.js
+++ sig.js
@@ -3,6 +3,6 @@ let url = $request.url;
 var cons = "Account/GetUser";
 if(url.indexOf(cons) != -1) {
     obj.vipKind = 4,
-    obj.vipDate= "2024-04-27T10:10:20.79"
+    obj.vipDate= "2028-04-27T10:10:20.79"
 }
 $done({body: JSON.stringify(obj)});
